1ac3bb2a01b74a0d080486fa265ccb136aa98efc
koki <2788293436@qq.com>
2021-01-28 15:37:32 +0800

    fix: remove hd & fix qrcode link

--- .umirc.js
+++ .umirc.js
@@ -10,5 +10,11 @@ export default defineConfig({
   publicPath: '/react-h5-calendar/',
   base: '/react-h5-calendar/',
   hash: true,
+  themeConfig: {
+    hd: {
+      rules: [], // 禁用高清方案
+      // 更多 rule 配置访问 https://github.com/umijs/dumi/blob/master/packages/theme-mobile/src/typings/config.d.ts#L7
+    },
+  },
   // more config: https://d.umijs.org/config
 })
